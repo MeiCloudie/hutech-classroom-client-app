c741763ed770637c8de26a791c843a3be003cb76
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-05-15 22:28:13 +0700

    💡 feat: Create Home Page

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Layout />
-      <h1>Hello</h1>
     </div>
   );
 }
--- src/app/layout/components/Layout.tsx
+++ src/app/layout/components/Layout.tsx
@@ -6,6 +6,7 @@ import Typography from '@mui/material/Typography';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
+import HomePage from '../../../features/home/HomePage';
 
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -36,22 +37,7 @@ const Layout = () => {
 
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
-        <Typography paragraph>
-          AAAA
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <HomePage />
       </Box>
 
       <Footer />
