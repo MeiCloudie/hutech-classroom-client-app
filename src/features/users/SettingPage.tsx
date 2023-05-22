66220845d302adfa5ad03901e3e5b7c4f413c811
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-05-22 15:19:54 +0700

    🟢 feat: Create Setting Page

--- src/features/users/SettingPage.tsx
+++ src/features/users/SettingPage.tsx
@@ -1,7 +1,97 @@
-const SettingPage = () => {
-    return (
-        <h1>SettingPage</h1>
-    )
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { CardMedia } from "@mui/material";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
 }
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`,
+  };
+}
+
+const SettingPage = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        bgcolor: "background.paper",
+        display: "flex",
+        height: 500,
+      }}
+    >
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        sx={{ borderRight: 1, borderColor: "divider" }}
+      >
+        <Tab
+          label={
+            <CardMedia
+              component="img"
+              image="logoHutech.png"
+              alt="Ảnh không tồn tại"
+              sx={{
+                width: "300px",
+                height: "auto",
+                margin: "10px",
+              }}
+            />
+          }
+          {...a11yProps(0)}
+        />
+        <Tab label="Tài khoản" {...a11yProps(1)} />
+        <Tab label="Mật khẩu" {...a11yProps(2)} />
+        {/* <Tab label="Hình đại diện" {...a11yProps(2)} /> */}
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </Box>
+  );
+};
 
 export default SettingPage;
