992f64f9bbe113b9df310f5417ac9aeef78b0cf8
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-05-22 22:07:40 +0700

    🟢 feat: Delete Member models

--- src/app/common/models/Member.ts
+++
@@ -1,9 +0,0 @@
-import { Faculty } from "../../models/Faculty";
-
-export default interface Member {
-    userName: string,
-    email: string,
-    firstName: string,
-    lastName: string,
-    faculty?: Faculty
-}
