0c72c14a9b52af9d7c64a91d35def41d73ee608f
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-05-25 15:44:19 +0700

    🟤 chore: Remove redundant code

--- src/features/classrooms/posts/list/CreatePostDialog.tsx
+++ src/features/classrooms/posts/list/CreatePostDialog.tsx
@@ -1,10 +1,6 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
-import ListItemText from "@mui/material/ListItemText";
-import ListItem from "@mui/material/ListItem";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
@@ -105,19 +101,6 @@ const CreatePostDialog = () => {
             }
           />
         </DialogContent>
-
-        {/* <List>
-          <ListItem button>
-            <ListItemText primary="Phone ringtone" secondary="Titania" />
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemText
-              primary="Default notification ringtone"
-              secondary="Tethys"
-            />
-          </ListItem>
-        </List> */}
       </Dialog>
     </Box>
   );
