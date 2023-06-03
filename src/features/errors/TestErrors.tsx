03509677ea4a7a183083cacfa35d13e7611e251a
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-06-03 22:38:08 +0700

    🟡 feat: Update Errors Page

--- src/features/errors/TestErrors.tsx
+++ src/features/errors/TestErrors.tsx
@@ -1,7 +1,94 @@
+import { Box, Button, ButtonGroup, Typography } from "@mui/material";
+import axios from "axios";
+import React, { useState } from "react";
+import ValidationError from "./ValidationError";
+
 const TestErrors = () => {
-    return (
-        <h1>Test Errors Page</h1>
-    )
-}
+  const baseUrl = process.env.REACT_APP_HUTECH_CLASSROOM_BASE_URL;
+  const [errors, setErrors] = useState(null);
+
+  function handleNotFound() {
+    axios
+      .get(baseUrl + "results/not-found")
+      .catch((err) => console.log(err.response));
+  }
+
+  function handleBadRequest() {
+    axios
+      .get(baseUrl + "results/bad-request")
+      .catch((err) => console.log(err.response));
+  }
+
+  function handleNoContent() {
+    axios
+      .get(baseUrl + "results/no-content")
+      .catch((err) => console.log(err.response));
+  }
+
+  function handleUnauthorized() {
+    axios
+      .get(baseUrl + "results/unauthorized")
+      .catch((err) => console.log(err.response));
+  }
+
+  function handleConflict() {
+    axios
+      .get(baseUrl + "results/conflict")
+      .catch((err) => console.log(err.response));
+  }
+
+  function handleForbid() {
+    axios
+      .get(baseUrl + "results/forbid")
+      .catch((err) => console.log(err.response));
+  }
+
+  function handleInternalServerError() {
+    axios
+      .get(baseUrl + "results/internal-server-error")
+      .catch((err) => console.log(err.response));
+  }
+
+  function handleValidationError() {
+    axios.post(baseUrl + "classrooms", {}).catch((err) => setErrors(err));
+  }
+
+  return (
+    <React.Fragment>
+      <Typography variant="h2" component="h2" fontWeight="bold" gutterBottom>
+        Test Error component
+      </Typography>
+      <Box mt={2} mb={2}>
+        <ButtonGroup fullWidth variant="contained" size="large">
+          <Button onClick={handleNotFound} color="primary">
+            404 - Not Found
+          </Button>
+          <Button onClick={handleBadRequest} color="primary">
+            400 - Bad Request
+          </Button>
+          <Button onClick={handleNoContent} color="primary">
+            204 - No Content
+          </Button>
+          <Button onClick={handleUnauthorized} color="primary">
+            401 - Unauthorized
+          </Button>
+          <Button onClick={handleConflict} color="primary">
+            409 - Conflict
+          </Button>
+          <Button onClick={handleForbid} color="primary">
+            403 - Forbid
+          </Button>
+          <Button onClick={handleInternalServerError} color="primary">
+            500 - Internal Server Error
+          </Button>
+          <Button onClick={handleValidationError} color="primary">
+            422 - Validation Error
+          </Button>
+        </ButtonGroup>
+      </Box>
+      {errors && <ValidationError errors={errors} />}
+    </React.Fragment>
+  );
+};
 
 export default TestErrors;
--- src/features/errors/ValidationError.tsx
+++ src/features/errors/ValidationError.tsx
@@ -1,7 +1,17 @@
-const ValidationError = () => {
-    return (
-        <h1>Validation Error Page</h1>
-    )
-}
+import { Typography, Box, List, ListItem } from "@mui/material";
+
+const ValidationError = ({ errors }: { errors: any[] }) => {
+  return (
+    <Box mt={2}>
+      <Typography variant="body1" color="error">
+        <List>
+          {errors.map((err: any, i: any) => (
+            <ListItem key={i}>{err}</ListItem>
+          ))}
+        </List>
+      </Typography>
+    </Box>
+  );
+};
 
 export default ValidationError;
