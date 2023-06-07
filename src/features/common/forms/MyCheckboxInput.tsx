423fd4f4bdb6cb3b8d386e42e724977fc5f29c2e
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-06-07 22:17:56 +0700

    🟡 feat: Update My CheckBox Input

--- src/features/common/forms/MyCheckboxInput.tsx
+++ src/features/common/forms/MyCheckboxInput.tsx
@@ -1,33 +1,26 @@
-import { Checkbox } from "@mui/material";
+import { Checkbox, FormControlLabel } from "@mui/material";
 import { useField } from "formik";
-import { v1 as uuidv1 } from 'uuid';
+import { v1 as uuidv1 } from "uuid";
 
 const MyCheckboxInput = ({ label, ...props }: any) => {
-    const [field] = useField(props);
-    return (
-        <div>
-            <Checkbox
-                {...field}
-                {...props}
-                checked={field.value}
-                id={`${uuidv1()}-${props.name}-text-form`}
-                variant="outlined"
-                sx={{
-                    mt: 2,
-                    width: "100%",
-                    maxWidth: "100ch",
-                    "@media (max-width: 400px)": {
-                        width: "120ch",
-                        maxWidth: "100%",
-                    },
-                    "@media (min-width: 1280px)": {
-                        width: "100ch",
-                        maxWidth: "100%",
-                    },
-                }}
-            />
-        </div>
-    );
+  const [field] = useField(props);
+  return (
+    <div>
+      <FormControlLabel
+        control={
+          <Checkbox
+            {...field}
+            {...props}
+            checked={field.value}
+            id={`${uuidv1()}-${props.name}-text-form`}
+            variant="outlined"
+            aria-label={label}
+          />
+        }
+        label={label}
+      />
+    </div>
+  );
 };
 
 export default MyCheckboxInput;
