54d0988089d0b95cdaab0f4391e440edd7bf50c0
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-12-12 20:26:31 +0700

    feat(auth): Create Dialog - reminder

--- src/app/router/RequireAuth.tsx
+++ src/app/router/RequireAuth.tsx
@@ -1,13 +1,66 @@
-import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { useStore } from "../stores/store";
+import React from "react"
+import { Link, Navigate, Outlet, useLocation } from "react-router-dom"
+import { useStore } from "../stores/store"
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material"
 
 export default function RequireAuth() {
-    const { userStore: { isLoggedIn } } = useStore()
-    const location = useLocation()
+  const {
+    userStore: { isLoggedIn, user, isStudent },
+  } = useStore()
+  const location = useLocation()
 
-    if (!isLoggedIn) {
-        return <Navigate to='/' state={{ from: location }} />
+  const [open, setOpen] = React.useState(false)
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  React.useEffect(() => {
+    if (isLoggedIn && isStudent) {
+      setOpen(true)
     }
+  }, [isLoggedIn, isStudent])
 
-    return <Outlet />
+  if (!isLoggedIn) {
+    return <Navigate to="/" state={{ from: location }} />
+  }
+
+  return (
+    <>
+      <Outlet />
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-reminder"
+        aria-describedby="alert-dialog-remind-update-email"
+      >
+        <DialogTitle id="alert-dialog-reminder">{"NHẮC NHỞ"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-remind-update-email">
+            {`Bạn còn sử dụng email "${user?.email}" không? Nếu không hãy cập nhật lại email mới!`}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} sx={{ color: "gray" }}>
+            ĐỂ SAU
+          </Button>
+          <Button
+            onClick={handleClose}
+            autoFocus
+            component={Link}
+            to={`/settings`}
+          >
+            THỰC HIỆN NGAY
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  )
 }
