5ac3a94815e2b26fa20863734f40c39cc3b7465c
Thai Nguyen Hong <nhthai01@axonactive.com>
2025-04-09 14:28:21 +0700

    fix: buttons dont have space between

--- src/features/classrooms/exercises/answers/form/AnswerForm.tsx
+++ src/features/classrooms/exercises/answers/form/AnswerForm.tsx
@@ -1,134 +1,153 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography } from '@mui/material';
 
-import * as Yup from "yup";
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { Answer, AnswerFormValues } from "../../../../../app/models/Answer";
-import { useStore } from "../../../../../app/stores/store";
-import EntityForm from "../../../../common/forms/EntityForm";
-import { InputType } from "../../../../../app/layout/enums/InputTypes";
+import * as Yup from 'yup';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Answer, AnswerFormValues } from '../../../../../app/models/Answer';
+import { useStore } from '../../../../../app/stores/store';
+import EntityForm from '../../../../common/forms/EntityForm';
+import { InputType } from '../../../../../app/layout/enums/InputTypes';
 
 interface AnswerFormProps {
-  handleClose: () => void;
-  answer?: Answer;
+    handleClose: () => void;
+    answer?: Answer;
 }
 
 const AnswerForm = (props: AnswerFormProps) => {
-  const { exerciseId, answerId } = useParams<{
-    exerciseId: string;
-    answerId: string;
-  }>();
-  const { answerStore, userStore } = useStore();
-  const [answerFormValues, setAnswerFormValues] = useState<AnswerFormValues>(
-    new AnswerFormValues()
-  );
+    const { exerciseId, answerId } = useParams<{
+        exerciseId: string;
+        answerId: string;
+    }>();
+    const { answerStore, userStore } = useStore();
+    const [answerFormValues, setAnswerFormValues] = useState<AnswerFormValues>(
+        new AnswerFormValues()
+    );
 
-  useEffect(() => {
-    if (props.answer) setAnswerFormValues(new AnswerFormValues(props.answer));
-    else if (answerId)
-      answerStore.get(answerId).then(() => {
-        if (answerStore.selectedItem) {
-          setAnswerFormValues(new AnswerFormValues(answerStore.selectedItem));
-        }
-      });
-  }, [answerId, answerStore, props.answer]);
+    useEffect(() => {
+        if (props.answer)
+            setAnswerFormValues(new AnswerFormValues(props.answer));
+        else if (answerId)
+            answerStore.get(answerId).then(() => {
+                if (answerStore.selectedItem) {
+                    setAnswerFormValues(
+                        new AnswerFormValues(answerStore.selectedItem)
+                    );
+                }
+            });
+    }, [answerId, answerStore, props.answer]);
 
-  return (
-    <Box>
-      <Box
-        sx={{
-          bgcolor: "#f5f5f5",
-          width: "100%",
-          p: 2,
-          border: "1px solid #e8e8e8",
-          borderRadius: "5px",
-          transition: "transform 0.3s, border-color 0.3s, box-shadow 0.3s",
-          "&:hover": {
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.4)",
-            transform: "translateY(-4px)",
-          },
-        }}
-      >
-        <Typography
-          variant="h4"
-          gutterBottom
-          sx={{
-            fontWeight: 600,
-            color: (theme) => theme.palette.primary.main,
-            textAlign: "center",
-          }}
-        >
-          THÔNG TIN CÂU TRẢ LỜI
-        </Typography>
-        <Box sx={{ display: "flex", flexGrow: 1, justifyContent: "center" }}>
-          <EntityForm<AnswerFormValues>
-            initialEntityFormValues={answerFormValues}
-            selectionFields={[]}
-            validateObject={{
-              description: Yup.string().max(
-                500,
-                "Mô tả không được vượt quá 500 ký tự!"
-              ),
-              link: Yup.string().max(
-                2000,
-                "Nội dung không được vượt quá 2000 ký tự!"
-              ),
-              score: Yup.number()
-                .transform((value, originalValue) => {
-                  return originalValue === undefined ? -1 : value;
-                })
-                .typeError("Điểm phải ở dạng số!"),
-            }}
-            fieldConfigs={[
-              {
-                fieldKey: "description",
-                props: {
-                  label: "Mô tả câu trả lời",
-                  placeholder: "Hãy nhập mô tả câu trả lời tại đây!",
-                  type: InputType.Textarea,
-                },
-              },
-              {
-                fieldKey: "link",
-                props: {
-                  label: "Liên Kết",
-                  placeholder: "Hãy thêm đường dẫn liên kết tại đây!",
-                  rows: 5,
-                },
-              },
-              {
-                fieldKey: "score",
-                props: {
-                  label: "Điểm số",
-                  placeholder: "Hãy thêm điểm số bài tập tại đây!",
-                },
-              },
-            ]}
-            excludeFields={["exerciseId", "userId", "score"]}
-            onSubmit={(entityFormValues) => {
-              console.log(entityFormValues);
-              if (entityFormValues.id) {
-                answerStore
-                  .update(entityFormValues.id, entityFormValues)
-                  .then(() => {
-                    props.handleClose();
-                  });
-              } else {
-                answerStore.create(entityFormValues).then(() => {
-                  props.handleClose();
-                });
-              }
-            }}
-            onCancel={props.handleClose}
-            onSetAdditionalValues={(answerFormValues) => {
-              answerFormValues.exerciseId = exerciseId;
-              answerFormValues.userId = userStore.user?.id;
-            }}
-          />
+    return (
+        <Box>
+            <Box
+                sx={{
+                    bgcolor: '#f5f5f5',
+                    width: '100%',
+                    p: 2,
+                    border: '1px solid #e8e8e8',
+                    borderRadius: '5px',
+                    transition:
+                        'transform 0.3s, border-color 0.3s, box-shadow 0.3s',
+                    '&:hover': {
+                        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.4)',
+                        transform: 'translateY(-4px)',
+                    },
+                }}
+            >
+                <Typography
+                    variant='h4'
+                    gutterBottom
+                    sx={{
+                        fontWeight: 600,
+                        color: (theme) => theme.palette.primary.main,
+                        textAlign: 'center',
+                    }}
+                >
+                    THÔNG TIN CÂU TRẢ LỜI
+                </Typography>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexGrow: 1,
+                        justifyContent: 'center',
+                    }}
+                >
+                    <EntityForm<AnswerFormValues>
+                        initialEntityFormValues={answerFormValues}
+                        selectionFields={[]}
+                        validateObject={{
+                            description: Yup.string().max(
+                                500,
+                                'Mô tả không được vượt quá 500 ký tự!'
+                            ),
+                            link: Yup.string().max(
+                                2000,
+                                'Nội dung không được vượt quá 2000 ký tự!'
+                            ),
+                            score: Yup.number()
+                                .transform((value, originalValue) => {
+                                    return originalValue === undefined
+                                        ? -1
+                                        : value;
+                                })
+                                .typeError('Điểm phải ở dạng số!'),
+                        }}
+                        fieldConfigs={[
+                            {
+                                fieldKey: 'description',
+                                props: {
+                                    label: 'Mô tả câu trả lời',
+                                    placeholder:
+                                        'Hãy nhập mô tả câu trả lời tại đây!',
+                                    type: InputType.Textarea,
+                                },
+                            },
+                            {
+                                fieldKey: 'link',
+                                props: {
+                                    label: 'Liên Kết',
+                                    placeholder:
+                                        'Hãy thêm đường dẫn liên kết tại đây!',
+                                    rows: 5,
+                                },
+                            },
+                            {
+                                fieldKey: 'score',
+                                props: {
+                                    label: 'Điểm số',
+                                    placeholder:
+                                        'Hãy thêm điểm số bài tập tại đây!',
+                                },
+                            },
+                        ]}
+                        excludeFields={['exerciseId', 'userId', 'score']}
+                        onSubmit={(entityFormValues) => {
+                            if (entityFormValues.id) {
+                                answerStore
+                                    .update(
+                                        entityFormValues.id,
+                                        entityFormValues
+                                    )
+                                    .then(() => {
+                                        props.handleClose();
+                                    });
+                            } else {
+                                answerStore
+                                    .create(entityFormValues)
+                                    .then(() => {
+                                        props.handleClose();
+                                    });
+                            }
+                        }}
+                        onCancel={props.handleClose}
+                        onSetAdditionalValues={(answerFormValues) => {
+                            answerFormValues.exerciseId = exerciseId;
+                            answerFormValues.userId = userStore.user?.id;
+                        }}
+                    />
+                </Box>
+            </Box>
         </Box>
-      </Box>
-    </Box>
-  );
+    );
 };
 
 export default AnswerForm;
--- src/features/common/forms/EntityForm.tsx
+++ src/features/common/forms/EntityForm.tsx
@@ -1,208 +1,220 @@
-import { EntityFormValues } from "../../../app/common/models/Entity";
-import * as Yup from "yup";
-import { Formik, FormikHelpers } from "formik";
-import { Box, Button, Stack } from "@mui/material";
-import MyTextInput from "./MyTextInput";
-import humanizeString from "humanize-string";
-import MyCheckboxInput from "./MyCheckboxInput";
-import MyPasswordInput from "./MyPasswordInput";
-import MySelectionInput from "./MySelectionInput";
-import MyTextAreaInput from "./MyTextAreaInput";
-import MyDateInput from "./MyDateInput";
-import { InputType } from "../../../app/layout/enums/InputTypes";
+import { EntityFormValues } from '../../../app/common/models/Entity';
+import * as Yup from 'yup';
+import { Formik, FormikHelpers } from 'formik';
+import { Box, Button, Stack } from '@mui/material';
+import MyTextInput from './MyTextInput';
+import humanizeString from 'humanize-string';
+import MyCheckboxInput from './MyCheckboxInput';
+import MyPasswordInput from './MyPasswordInput';
+import MySelectionInput from './MySelectionInput';
+import MyTextAreaInput from './MyTextAreaInput';
+import MyDateInput from './MyDateInput';
+import { InputType } from '../../../app/layout/enums/InputTypes';
 
-interface EntityFormProps<
-  TEntityFormValues extends EntityFormValues
-> {
-  initialEntityFormValues: TEntityFormValues;
-  selectionFields: {
-    fieldKey: string;
-    options: { label: string; value: any }[];
-  }[];
-  fieldConfigs: { fieldKey: string; props: FieldProps }[];
-  excludeFields: string[];
-  validateObject: {};
-  onSubmit: (entityFormValues: TEntityFormValues) => void;
-  onCancel?: () => void;
-  onSetAdditionalValues: (entityFormValues: TEntityFormValues) => void;
+interface EntityFormProps<TEntityFormValues extends EntityFormValues> {
+    initialEntityFormValues: TEntityFormValues;
+    selectionFields: {
+        fieldKey: string;
+        options: { label: string; value: any }[];
+    }[];
+    fieldConfigs: { fieldKey: string; props: FieldProps }[];
+    excludeFields: string[];
+    validateObject: {};
+    onSubmit: (entityFormValues: TEntityFormValues) => void;
+    onCancel?: () => void;
+    onSetAdditionalValues: (entityFormValues: TEntityFormValues) => void;
 }
 
 export interface FieldProps {
-  placeholder: string;
-  label: string;
-  type?: InputType
-  rows?: number;
+    placeholder: string;
+    label: string;
+    type?: InputType;
+    rows?: number;
 }
-const EntityForm = <
-  TEntityFormValues extends EntityFormValues
->({
-  initialEntityFormValues,
-  onSubmit,
-  onCancel,
-  onSetAdditionalValues,
-  selectionFields,
-  fieldConfigs,
-  excludeFields,
-  validateObject,
+const EntityForm = <TEntityFormValues extends EntityFormValues>({
+    initialEntityFormValues,
+    onSubmit,
+    onCancel,
+    onSetAdditionalValues,
+    selectionFields,
+    fieldConfigs,
+    excludeFields,
+    validateObject,
 }: EntityFormProps<TEntityFormValues>) => {
+    const validationSchema = Yup.object(validateObject);
+    const handleFormSubmit = (
+        entityFormValues: TEntityFormValues,
+        actions: FormikHelpers<TEntityFormValues>
+    ) => {
+        onSetAdditionalValues(entityFormValues);
+        onSubmit(entityFormValues);
+        if (!initialEntityFormValues.id) actions.resetForm();
+        actions.setSubmitting(false);
+    };
 
-  const validationSchema = Yup.object(validateObject);
-  const handleFormSubmit = (
-    entityFormValues: TEntityFormValues,
-    actions: FormikHelpers<TEntityFormValues>
-  ) => {
-    onSetAdditionalValues(entityFormValues);
-    onSubmit(entityFormValues);
-    if (!initialEntityFormValues.id) actions.resetForm();
-    actions.setSubmitting(false);
-  };
-
-  const getInputComponent = ([key, value]: [string, any], index: number) => {
-    const fieldConfig = fieldConfigs.find((field) => field.fieldKey === key);
-    const label = fieldConfig?.props.label ?? humanizeString(key);
-    const placeholder = fieldConfig?.props.placeholder ?? key;
-    const isTextarea = fieldConfig?.props.type === InputType.Textarea;
-    const isDate = fieldConfig?.props.type === InputType.Date;
-    if (excludeFields.includes(key)) return null;
-    const selectionField = selectionFields.find((field) => field.fieldKey === key);
-    if (selectionField)
-      return (
-        <MySelectionInput
-          key={index}
-          icon={null}
-          label={label}
-          name={selectionField.fieldKey}
-          options={selectionField.options}
-        />
-      );
-    if (key.toLowerCase().includes("password"))
-      return (
-        <MyPasswordInput
-          name={key}
-          label={label}
-          key={index}
-          placeholder={placeholder}
-        />
-      );
-    if (!initialEntityFormValues.id && key.toLowerCase() === "id") return null;
-    if (initialEntityFormValues.id && key.toLowerCase() === "id")
-      return (
-        <MyTextInput
-          name={key}
-          label={label}
-          disabled
-          key={index}
-          placeholder={placeholder}
-        />
-      );
-      if (isDate || value instanceof Date) {
-        return (
-          <MyDateInput
-            name={key}
-            label={label}
-            key={index}
-            placeholder={placeholder}
-          />
+    const getInputComponent = ([key, value]: [string, any], index: number) => {
+        const fieldConfig = fieldConfigs.find(
+            (field) => field.fieldKey === key
         );
-      }
-    if (typeof value === "string" && !isTextarea) {
-      const rows = fieldConfig?.props.rows ?? 1;
-      const isMultiline = rows > 1
-      return (
-        <MyTextInput
-          name={key}
-          label={label}
-          key={index}
-          rows={rows}
-          multiline={isMultiline}
-          placeholder={placeholder}
-        />
-      );
-    }
-    if (typeof value === "string" && isTextarea)
-      return (
-        <MyTextAreaInput
-          name={key}
-          label={label}
-          key={index}
-          placeholder={placeholder}
-        />
-      );
-    
-        
-    if (typeof value === "number")
-      return (
-        <MyTextInput
-          name={key}
-          label={label}
-          key={index}
-          placeholder={placeholder}
-          type="number"
-        />
-      );
-    if (typeof value === "boolean")
-      return (
-        <MyCheckboxInput
-          name={key}
-          label={label}
-          placeholder={placeholder}
-          key={index}
-        />
-      );
+        const label = fieldConfig?.props.label ?? humanizeString(key);
+        const placeholder = fieldConfig?.props.placeholder ?? key;
+        const isTextarea = fieldConfig?.props.type === InputType.Textarea;
+        const isDate = fieldConfig?.props.type === InputType.Date;
+        if (excludeFields.includes(key)) return null;
+        const selectionField = selectionFields.find(
+            (field) => field.fieldKey === key
+        );
+        if (selectionField)
+            return (
+                <MySelectionInput
+                    key={index}
+                    icon={null}
+                    label={label}
+                    name={selectionField.fieldKey}
+                    options={selectionField.options}
+                />
+            );
+        if (key.toLowerCase().includes('password'))
+            return (
+                <MyPasswordInput
+                    name={key}
+                    label={label}
+                    key={index}
+                    placeholder={placeholder}
+                />
+            );
+        if (!initialEntityFormValues.id && key.toLowerCase() === 'id')
+            return null;
+        if (initialEntityFormValues.id && key.toLowerCase() === 'id')
+            return (
+                <MyTextInput
+                    name={key}
+                    label={label}
+                    disabled
+                    key={index}
+                    placeholder={placeholder}
+                />
+            );
+        if (isDate || value instanceof Date) {
+            return (
+                <MyDateInput
+                    name={key}
+                    label={label}
+                    key={index}
+                    placeholder={placeholder}
+                />
+            );
+        }
+        if (typeof value === 'string' && !isTextarea) {
+            const rows = fieldConfig?.props.rows ?? 1;
+            const isMultiline = rows > 1;
+            return (
+                <MyTextInput
+                    name={key}
+                    label={label}
+                    key={index}
+                    rows={rows}
+                    multiline={isMultiline}
+                    placeholder={placeholder}
+                />
+            );
+        }
+        if (typeof value === 'string' && isTextarea)
+            return (
+                <MyTextAreaInput
+                    name={key}
+                    label={label}
+                    key={index}
+                    placeholder={placeholder}
+                />
+            );
 
-    return null;
-  };
+        if (typeof value === 'number')
+            return (
+                <MyTextInput
+                    name={key}
+                    label={label}
+                    key={index}
+                    placeholder={placeholder}
+                    type='number'
+                />
+            );
+        if (typeof value === 'boolean')
+            return (
+                <MyCheckboxInput
+                    name={key}
+                    label={label}
+                    placeholder={placeholder}
+                    key={index}
+                />
+            );
 
-  return (
-    <Formik
-      key="formik-form"
-      enableReinitialize
-      initialValues={initialEntityFormValues}
-      onSubmit={handleFormSubmit}
-      validationSchema={validationSchema}
-    >
-      {({ handleSubmit, isSubmitting }) => (
-        <Box
-          component="form"
-          noValidate
-          autoComplete="true"
-          onSubmit={handleSubmit}
+        return null;
+    };
+
+    return (
+        <Formik
+            key='formik-form'
+            enableReinitialize
+            initialValues={initialEntityFormValues}
+            onSubmit={handleFormSubmit}
+            validationSchema={validationSchema}
         >
-          {Object.entries(initialEntityFormValues).map(getInputComponent)}
+            {({ handleSubmit, isSubmitting }) => (
+                <Box
+                    component='form'
+                    noValidate
+                    autoComplete='true'
+                    onSubmit={handleSubmit}
+                >
+                    {Object.entries(initialEntityFormValues).map(
+                        getInputComponent
+                    )}
 
-          <Stack sx={{ width: "100%" }} spacing={2}>
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "flex-end",
-              }}
-            >
-              <Stack spacing={2} direction="row">
-                <Button
-                  onClick={onCancel}
-                  variant="contained"
-                  sx={{ m: "10px" }}
-                >
-                  HUỶ
-                </Button>
-              </Stack>
-              <Stack spacing={2} direction="row">
-                <Button
-                  type="submit"
-                  variant="contained"
-                  disabled={isSubmitting}
-                  sx={{ m: "10px 0" }}
-                >
-                  {initialEntityFormValues.id ? "CẬP NHẬT" : "TẠO"}
-                </Button>
-              </Stack>
-            </div>
-          </Stack>
-        </Box>
-      )}
-    </Formik>
-  );
+                    <Stack sx={{ width: '100%' }} spacing={2}>
+                        <Box
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'flex-end',
+                            }}
+                            sx={{ mt: 3 }}
+                        >
+                            <Stack
+                                direction='row'
+                                spacing={2}
+                                sx={{
+                                    width: '100%',
+                                    justifyContent: 'flex-end',
+                                    mt: 1,
+                                }}
+                            >
+                                <Button
+                                    onClick={onCancel}
+                                    variant='contained'
+                                    fullWidth
+                                    sx={{ maxWidth: 60 }} // Set a max width to make buttons more proportional
+                                >
+                                    HUỶ
+                                </Button>
+                                <Button
+                                    type='submit'
+                                    variant='contained'
+                                    disabled={isSubmitting}
+                                    fullWidth
+                                    sx={{ maxWidth: 60 }} // Set a max width to make buttons more proportional
+                                >
+                                    {initialEntityFormValues.id
+                                        ? 'CẬP NHẬT'
+                                        : 'TẠO'}
+                                </Button>
+                            </Stack>
+                        </Box>
+                    </Stack>
+                </Box>
+            )}
+        </Formik>
+    );
 };
 
 export default EntityForm;
